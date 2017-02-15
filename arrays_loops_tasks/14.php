2019dba66ad014a40f16856fd61142869f41c964
ILIVAN <ilivan@i.ua>
2017-02-15 22:53:18 +0200

    Revert "Bugs have been fixed (Task 14, 15)"
    This reverts commit 1a720f0761b4f1155fdb1f739d83ceaa9eec9f52.

--- arrays_loops_tasks/14.php
+++ arrays_loops_tasks/14.php
@@ -7,14 +7,14 @@
  <body>
     <?php
       $arr = [4, 2, 5, 19, 13, 0, 10];
+      $e = rand(2,4);
       $i=0;
-      foreach ($arr as $e)
-         if ($e == 2 || $e == 3 || $e == 4)
-          $i=1;
-
+      foreach ($arr as $key => $value) {
+        if($value == $e)
+          {echo "Есть!<br>" . "e = " . $e;$i=1;break;}
+      }
       if($i==0)
-      echo "Нет!<br>";
-      else echo "Есть!<br>";
+      echo "Нет!<br>" . "e = " . $e;
     ?>
  </body>
 </html>
--- arrays_loops_tasks/15.php
+++ arrays_loops_tasks/15.php
@@ -7,7 +7,7 @@
  <body>
     <?php
       $arr = [4, 2, 5, 19, 13, 0, 10];
-      $count = 0;
+      $count = 1;
       foreach ($arr as $key => $value)
        $count++;
       echo "В массиве {$count} элементов.";
